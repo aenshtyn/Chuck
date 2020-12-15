9785e85d8c86aa2c7f6467c2935948155d3ab4c0
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-12-15 18:10:22 +0300

    cleared unnecessary files from boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [jokes, setJokes] = useState([])
+  const [query, setQuery] = useState('categories')
+
+  useEffect(() => {
+    getCategories();
+  })
+
+  const getCategories = async () => {
+    const response = await axios.get('https://api.chucknorris.io/jokes/search?query=${query}')
+    setJokes(response.data.value)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    getCategories();
+  }
+
+
+  return (
+     <div>
+          <form onSubmit={handleSearch}>
+              <input type = "text"
+              onChange={e => setQuery(e.target.value)}
+              value={query}/>
+
+
+<button type = "submit"> Search </button>
+          </form>
+
+          {jokes.map(joke => {
+              return (
+                  <p> {joke.value} </p>
+              )
+          })}
+        </div>
+      );
+}
 export default App;
