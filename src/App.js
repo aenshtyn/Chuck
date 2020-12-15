d3435316bf094e07ea7bcc7de92a3d74d1f92c8e
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-12-15 19:30:36 +0300

    able to display a joke on the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-  
+
 function App() {
 
-  const [jokes, setJokes] = useState([])
-  const [query, setQuery] = useState('categories')
-
-  useEffect(() => {
-    getCategories();
+  const [state, setState] = useState({
+    joke: '', date: '', category: '',
   })
 
-  const getCategories = async () => {
-    const response = await axios.get('https://api.chucknorris.io/jokes/search?query=${query}')
-    setJokes(response.data.value)
+  useEffect( () => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const result = await axios.get('https://api.chucknorris.io/jokes/random');
+    console.log(result.data.value);
+    console.log(result.data.updated_at);
+    setState({
+      ...state,
+      joke: result.data.value,
+      date: result.data.updated_at,
+      category: result.data.categories
+    });
   }
 
-  const handleSearch = (e) => {
-    e.preventDefault()
-    getCategories();
-  }
+  // const getCategories = async () => {
+  //   const response = await axios.get('https://api.chucknorris.io/jokes/search?query=${query}')
+  //   setJokes(response.data.value)
+  // }
+
+  // const handleSearch = (e) => {
+  //   e.preventDefault()
+  //   getCategories();
+  // }
 
 
   return (
@@ -28,7 +41,7 @@ function App() {
       <hr></hr>
 
       <h6 className="categories">Categories</h6>
-      <button className="btn btn-link btn-md">Food</button>
+      <button className="btn btn-link btn-md">{state.category}</button>
       <button className="btn btn-link btn-md">All Jokes</button>
 
       <hr></hr>
@@ -36,13 +49,13 @@ function App() {
       <div className="card">
         <div className="card-header"> Joke </div>
 
-          <div className = "card-body"> <p>Joke No. 1</p>
-          <footer class="blockquote-footer">Created on <cite title="date"> date  </cite></footer> </div>
+        <div className="card-body"> <p> {state.joke}</p>
+          <footer class="blockquote-footer">Created on <cite title="date"> { state.date }  </cite></footer> </div>
 
-  
+
       </div>
     </div>
-    
-      );
+
+  );
 }
 export default App;
